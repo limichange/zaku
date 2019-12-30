ba2513c4d1a5e258a49f8184d3261604ac63f716
limichange <limichange@hotmail.com>
2019-12-30 13:02:03 +0800

    Update

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,33 +1,31 @@
 const withCSS = require('@zeit/next-css')
 const withLess = require('@zeit/next-less')
 
-module.exports = 
-	withCSS(
-		withLess({
-			webpack(config) {
-				config.module.rules.forEach(rule => {
-					if (Array.isArray(rule.use)) {
-						rule.use.forEach(u => {
-							if (u.loader === 'css-loader' && u.options) {
-								delete u.options.minimize
-							}
-						})
-					}
-				})
+module.exports = withCSS(
+  withLess({
+    webpack(config) {
+      config.module.rules.forEach(rule => {
+        if (Array.isArray(rule.use)) {
+          rule.use.forEach(u => {
+            if (u.loader === 'css-loader' && u.options) {
+              delete u.options.minimize
+            }
+          })
+        }
+      })
 
-				return Object.assign(config, {
-					target: 'electron-renderer',
-					devtool: 'cheap-module-source-map',
-					plugins: config.plugins.filter(p => {
-						return p.constructor.name !== 'UglifyJsPlugin'
-					})
-				})
-			},
-			exportPathMap() {
-				return {
-					'/login': { page: '/login' },
-					'/main': { page: '/main' }
-				}
-			}
-		})
-	)
+      return Object.assign(config, {
+        target: 'electron-renderer',
+        devtool: 'cheap-module-source-map',
+        plugins: config.plugins.filter(p => {
+          return p.constructor.name !== 'UglifyJsPlugin'
+        })
+      })
+    },
+    exportPathMap() {
+      return {
+        '/main': { page: '/main' }
+      }
+    }
+  })
+)
