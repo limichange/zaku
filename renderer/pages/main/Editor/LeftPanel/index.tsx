64156b4baa3f7ff35bcf507980dd6efe4a23a595
limichange <limichange@hotmail.com>
2019-12-30 14:04:29 +0800

    Update

--- renderer/pages/main/Editor/LeftPanel/index.tsx
+++ renderer/pages/main/Editor/LeftPanel/index.tsx
@@ -10,13 +10,10 @@ export default function LeftPanel() {
   return (
     <Tabs defaultActiveKey='1' onChange={callback}>
       <TabPane tab='Tab 1' key='1'>
-        Content of Tab Pane 1
+        组件
       </TabPane>
       <TabPane tab='Tab 2' key='2'>
-        Content of Tab Pane 2
-      </TabPane>
-      <TabPane tab='Tab 3' key='3'>
-        Content of Tab Pane 3
+        属性
       </TabPane>
     </Tabs>
   )
