863425fad3a33fd4a14116be315aea2dee14b95c
limichange <limichange@hotmail.com>
2019-12-30 13:13:25 +0800

    Update

--- source/window/index.js
+++ source/window/index.js
@@ -2,47 +2,47 @@ const { app, BrowserWindow, Menu } = require('electron')
 const loadRoute = require('../utils/routes')
 
 exports.createLoginWin = async () => {
-	const win = new BrowserWindow({
-		title: app.name,
-		show: false,
-		width: 300,
-		height: 450,
-		titleBarStyle: 'hidden',
-		resizable: false,
-		webPreferences: {
-			nodeIntegration: true,
-			webSecurity: false
-		}
-	})
+  const win = new BrowserWindow({
+    title: app.name,
+    show: false,
+    width: 300,
+    height: 450,
+    titleBarStyle: 'hidden',
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false
+    }
+  })
 
-	win.on('ready-to-show', () => {
-		win.show()
-	})
+  win.on('ready-to-show', () => {
+    win.show()
+  })
 
-	loadRoute(win, 'login')
+  loadRoute(win, 'main')
 
-	return win
+  return win
 }
 
 exports.createMainWin = async () => {
-	const win = new BrowserWindow({
-		title: '春早',
-		show: false,
-		width: 1000,
-		height: 800,
-		// titleBarStyle: 'hidden',
-		// resizable: false,
-		webPreferences: {
-			nodeIntegration: true,
-			webSecurity: false
-		}
-	})
+  const win = new BrowserWindow({
+    title: 'zaku',
+    show: false,
+    width: 1000,
+    height: 800,
+    // titleBarStyle: 'hidden',
+    // resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false
+    }
+  })
 
-	win.on('ready-to-show', () => {
-		win.show()
-	})
+  win.on('ready-to-show', () => {
+    win.show()
+  })
 
-	loadRoute(win, 'main')
+  loadRoute(win, 'main')
 
-	return win
+  return win
 }
