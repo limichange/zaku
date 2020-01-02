ae6f66fc6c3d6ed6315ebdbdd6b951dd292b8048
limichange <limichange@hotmail.com>
2020-01-02 22:28:57 +0800

    Update

--- renderer/pages/main/Editor/LeftPanel/index.tsx
+++ renderer/pages/main/Editor/LeftPanel/index.tsx
@@ -4,14 +4,21 @@ import './index.less'
 export default function RightPanel() {
   const [collectedProps, drop] = useDrop({
     accept: 'a',
-    collect: (minoter: DropTargetMonitor) => ({
-      isOver: minoter.isOver()
-    })
+    collect: (minoter: DropTargetMonitor) => {
+      console.log(1)
+
+      return {
+        isOver: minoter.isOver()
+      }
+    }
   })
+
+  const components = []
 
   return (
     <div className='LeftPanel' ref={drop}>
       Drop Target
+      {components}
     </div>
   )
 }
