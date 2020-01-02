02307f004b288966fc201e6518f7a564ecf5acc4
limichange <limichange@hotmail.com>
2020-01-02 23:01:48 +0800

    Update

--- renderer/pages/main/Editor/LeftPanel/index.tsx
+++ renderer/pages/main/Editor/LeftPanel/index.tsx
@@ -1,19 +1,22 @@
 import { useDrop, DropTargetMonitor } from 'react-dnd'
+import { useState } from 'react'
 import './index.less'
 
 export default function RightPanel() {
+  const [components, setComponents] = useState([])
   const [collectedProps, drop] = useDrop({
     accept: 'a',
     collect: (minoter: DropTargetMonitor) => {
+      const isOver = minoter.isOver()
       console.log(1)
 
+      isOver && setComponents(components.concat([<input />]))
+
       return {
-        isOver: minoter.isOver()
+        isOver
       }
     }
   })
-
-  const components = []
 
   return (
     <div className='LeftPanel' ref={drop}>
