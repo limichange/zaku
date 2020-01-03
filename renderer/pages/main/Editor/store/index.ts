ee6c8c2986768070034942d5372ea564e004b2d2
limichange <limichange@hotmail.com>
2020-01-03 11:05:54 +0800

    Update

--- renderer/pages/main/Editor/store/index.ts
+++ renderer/pages/main/Editor/store/index.ts
@@ -10,5 +10,19 @@ const initialState = {
 let state = initialState
 
 const EditorStore = {
-  subscribe: setState => subject.subscribe(setState)
+  init: () => subject.next(state),
+  subscribe: setState => subject.subscribe(setState),
+  sendMessage: message => {
+    state = {
+      ...state,
+      data: [...state.data, message],
+      newDataCount: state.newDataCount + 1
+    }
+
+    subject.next(state)
+  },
+  clear: () => {
+    state = initialState
+    subject.next(state)
+  }
 }
