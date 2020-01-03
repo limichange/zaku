9192e0855098a54749f6c1d5fb51d236e6d25bf8
limichange <limichange@hotmail.com>
2020-01-03 17:59:27 +0800

    Update

--- renderer/pages/main/Editor/LeftPanel/index.tsx
+++ renderer/pages/main/Editor/LeftPanel/index.tsx
@@ -4,10 +4,12 @@ import { Button, Input, DatePicker } from 'antd'
 import Hover from './Hover'
 import editorStore from '../store/editorStore'
 import uuid from 'uuid'
+import useSubscribe from '../../../../hooks/useSubscribe'
 import './index.less'
 
 export default function RightPanel() {
   const [components, setComponents] = useState([])
+  const [editorState] = useSubscribe(editorStore)
   const [collectedProps, drop] = useDrop({
     accept: ['input', 'button', 'datePicker'],
     drop: (item, monitor) => {
--- renderer/pages/main/Editor/RightPanel/SettingPanel/index.tsx
+++ renderer/pages/main/Editor/RightPanel/SettingPanel/index.tsx
@@ -6,12 +6,15 @@ import editorStore from '../../store/editorStore'
 export default function SettingPanel() {
   const [editorState] = useSubscribe(editorStore)
 
+  const component = editorState.components.find(
+    item => editorState.key === item.key
+  )
+
   return (
     <div>
       <div>{editorState.key}</div>
-      <div>
-        <Button></Button>
-      </div>
+      <div>{component.type === 'Button' && <Button></Button>}</div>
+      <div>{component.type === 'Input' && <Input></Input>}</div>
     </div>
   )
 }
