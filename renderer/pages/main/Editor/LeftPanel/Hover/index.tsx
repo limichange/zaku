1e0c4d83c0c160cf9015cba3a786bb04fac4eeaf
limichange <limichange@hotmail.com>
2020-01-04 23:08:38 +0800

    update hover component display size

--- renderer/pages/main/Editor/LeftPanel/Hover/index.tsx
+++ renderer/pages/main/Editor/LeftPanel/Hover/index.tsx
@@ -1,8 +1,8 @@
-import uuid from 'uuid'
-import { useState, useRef, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import store from '../../store/editorStore'
 import useSubscribe from '../../../../../hooks/useSubscribe'
 import classnames from 'classnames'
+import React from 'react'
 import './index.less'
 
 interface Hover {
@@ -11,12 +11,13 @@ interface Hover {
 
 export default function Hover(props) {
   const [editorStoreState] = useSubscribe(store)
-  const ref = useRef(props.children)
   const { uuid: key } = props
+  const [width, setWidth] = useState(0)
+  const [height, setHeight] = useState(0)
 
-  useEffect(() => {
-    console.log(ref.current)
-  }, [])
+  const children = React.cloneElement(props.children, {
+    id: key
+  })
 
   function onClick() {
     store.setIndex('0')
@@ -24,11 +25,24 @@ export default function Hover(props) {
     props.onClick(key)
   }
 
+  useEffect(() => {
+    const { width, height } = document
+      .getElementById(key)
+      .getBoundingClientRect()
+
+    setHeight(height)
+    setWidth(width)
+  }, [])
+
   return (
     <div style={{ position: 'relative' }}>
-      {props.children}
+      {children}
       <div
         onClick={onClick}
+        style={{
+          width: `${width}px`,
+          height: `${height}px`
+        }}
         className={classnames({
           hover: true,
           selected: key === editorStoreState.key
