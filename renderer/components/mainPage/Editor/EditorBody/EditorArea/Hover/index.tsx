39196ca095ad9f2515d68c2d998a9af17d3868bf
limichange <limichange@hotmail.com>
2020-01-07 21:53:13 +0800

    fixed component display incorrect error

--- renderer/components/mainPage/Editor/EditorBody/EditorArea/Hover/index.tsx
+++ renderer/components/mainPage/Editor/EditorBody/EditorArea/Hover/index.tsx
@@ -14,7 +14,8 @@ export default function Hover(props) {
   const [editorStoreState] = useSubscribe(store)
   const [width, setWidth] = useState(0)
   const [height, setHeight] = useState(0)
-  const [display, setDisplay] = useState('block')
+  const [left, setLeft] = useState(0)
+  const [top, setTop] = useState(0)
   const [item, dragRef] = useDrag({ type: props.type, text: 'a' })
   const { uuid: key } = props
 
@@ -31,22 +32,26 @@ export default function Hover(props) {
   useEffect(() => {
     const dom = document.getElementById(key)
 
-    setDisplay(getComputedStyle(dom).display)
-
     setTimeout(() => {
-      const { width, height } = dom.getBoundingClientRect()
+      const { width, height, left, top } = dom.getBoundingClientRect()
 
+      setLeft(left)
+      setTop(top)
       setHeight(height)
       setWidth(width)
     }, 100)
   }, [])
 
   return (
-    <div ref={dragRef} style={{ position: 'relative', display }}>
+    <>
       {children}
       <div
+        ref={dragRef}
         onClick={onClick}
         style={{
+          position: 'fixed',
+          left: `${left}px`,
+          top: `${top}px`,
           width: `${width}px`,
           height: `${height}px`
         }}
@@ -54,6 +59,6 @@ export default function Hover(props) {
           [$style.hover]: true,
           [$style.selected]: key === editorStoreState.key
         })}></div>
-    </div>
+    </>
   )
 }
