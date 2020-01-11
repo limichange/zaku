0f97873c711c711c42884235475acb204b363277
limichange <limichange@hotmail.com>
2020-01-11 22:57:45 +0800

    refactor component panel

--- renderer/components/mainPage/Editor/EditorBody/EditorArea/RightPanel/ComponentsPanel/index.tsx
+++ renderer/components/mainPage/Editor/EditorBody/EditorArea/RightPanel/ComponentsPanel/index.tsx
@@ -1,57 +1,30 @@
-import Input from './components/Input'
-import Button from './components/Button'
-import DatePicker from './components/DatePicker'
 import $style from './index.less'
 import useDrag from '../../../../../../../hooks/useDrag'
+import configs from './config'
 
 const Item = props => {
   const [item, dragRef] = useDrag(props.config)
 
-  return <div ref={dragRef}>{props.children}</div>
+  return (
+    <div className={$style.Item} ref={dragRef}>
+      {props.children}
+    </div>
+  )
 }
 
 const ComponentsPanel: React.FC = function() {
   return (
     <div className={$style.ComponentsPanel}>
-      <Item
-        config={{
-          type: 'AntdInput',
-          tag: 'Input',
-          name: 'Input',
-          noChildren: true
-        }}>
-        <Input></Input>
-      </Item>
-      <Item
-        config={{
-          type: 'AntdButton',
-          tag: 'Button',
-          name: 'Button',
-          text: 'Button'
-        }}>
-        <Button></Button>
-      </Item>
-      <Item
-        config={{
-          noChildren: true,
-          type: 'AntdDatePicker',
-          tag: 'DatePicker',
-          name: 'DatePicker'
-        }}>
-        <DatePicker></DatePicker>
-      </Item>
-      <Item
-        config={{
-          type: 'AntdTooltip',
-          tag: 'Tooltip',
-          name: 'Tooltip',
-          noHover: true,
-          attributes: {
-            title: 'tst'
-          }
-        }}>
-        <div>Tooltip</div>
-      </Item>
+      {configs.map(config => {
+        return (
+          <Item config={config}>
+            <div className={$style.imgWrap}>
+              <img src={config.previewImage} alt='' />
+            </div>
+            <div>{config.name}</div>
+          </Item>
+        )
+      })}
     </div>
   )
 }
