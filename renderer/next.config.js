75dd4f429506acd97917e4358413b92e7d763f49
limichange <limichange@hotmail.com>
2020-01-13 13:41:24 +0800

    test assetPrefix

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -7,6 +7,7 @@ module.exports = withCss(
     lessLoaderOptions: {
       javascriptEnabled: true
     },
+    assetPrefix: '/zaku/',
     webpack(config) {
       config.module.rules = [
         ...config.module.rules,
