2b93a45aff1a51f9b3a1ec87b4f27ef54a30d5d6
limichange <limichange@hotmail.com>
2020-01-13 18:13:33 +0800

    fixed dragpreview position

--- renderer/components/DragPreview/index.tsx
+++ renderer/components/DragPreview/index.tsx
@@ -1,8 +1,14 @@
 import $style from './index.less'
+import { px } from '../../utils/style'
+
+let index = 5000
 
 export default function DragPreview(props) {
   return (
-    <div ref={props.ref} className={$style.previewWrap}>
+    <div
+      style={{ top: px((index += 30)) }}
+      ref={props.ref}
+      className={$style.previewWrap}>
       <div className={$style.preview}>{props.children}</div>
     </div>
   )
