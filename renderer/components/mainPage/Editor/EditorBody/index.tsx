ccf917d57f74df379f285b90582b062ae4a0b510
limichange <limichange@hotmail.com>
2020-01-18 14:07:57 +0800

    add EventEditor

--- renderer/components/mainPage/Editor/EditorBody/index.tsx
+++ renderer/components/mainPage/Editor/EditorBody/index.tsx
@@ -3,6 +3,7 @@ import Preview from './Preview'
 import EditorArea from './EditorArea'
 import { Tabs } from 'antd'
 import $style from './index.less'
+import EventEditor from './EventEditor'
 
 const { TabPane } = Tabs
 
@@ -17,10 +18,13 @@ export default function EditorBody() {
         <TabPane tab='UI' key='0'>
           <EditorArea></EditorArea>
         </TabPane>
-        <TabPane tab='Code' key='1'>
+        <TabPane tab='Event' key='2'>
+          <EventEditor></EventEditor>
+        </TabPane>
+        <TabPane tab='Code' key='3'>
           <CodeEditor></CodeEditor>
         </TabPane>
-        <TabPane tab='Preview' forceRender={true} key='2'>
+        <TabPane tab='Preview' forceRender={true} key='4'>
           <Preview></Preview>
         </TabPane>
       </Tabs>
