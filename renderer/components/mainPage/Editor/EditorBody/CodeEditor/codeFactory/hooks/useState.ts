b70e70bdd70be53539f70458bd81b37f78510199
limichange <limichange@hotmail.com>
2020-01-21 19:42:54 +0800

    add function name and type

--- renderer/components/mainPage/Editor/EditorBody/CodeEditor/codeFactory/hooks/useState.ts
+++ renderer/components/mainPage/Editor/EditorBody/CodeEditor/codeFactory/hooks/useState.ts
@@ -4,14 +4,21 @@ import {
   arrayPattern,
   callExpression,
   identifier,
-  stringLiteral
+  stringLiteral,
+  numericLiteral
 } from '@babel/types'
 
-export function generateUseState() {
+export function generateUseState(name: string, type: 'string' | 'number') {
+  name.toLocaleUpperCase()
+
+  const funcName = name[0].toUpperCase() + name.substr(1).toLowerCase()
+  const stateDefaultValue =
+    type === 'string' ? stringLiteral('""') : numericLiteral(0)
+
   return variableDeclaration('const', [
     variableDeclarator(
-      arrayPattern([identifier('value'), identifier('setValue')]),
-      callExpression(identifier('useState'), [stringLiteral('""')])
+      arrayPattern([identifier(name), identifier(`set${funcName}`)]),
+      callExpression(identifier('useState'), [stateDefaultValue])
     )
   ])
 }
